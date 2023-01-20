277f939fbaa815b7dd0fc4575ef774472a3e335f
Ankit-yadav09 <yadavankit46565@gmail.com>
2023-01-20 23:23:54 +0530

    landing  page

--- Frontend/src/Components/Navbar.jsx
+++ Frontend/src/Components/Navbar.jsx
@@ -19,6 +19,7 @@ const Navbar = () => {
             {/* ABOVE NAVBAR START */}
             <Box 
             // style={{border:"1px solid green"}}
+            
             >
                 <div className={styles.aboveNavDiv} >
 
