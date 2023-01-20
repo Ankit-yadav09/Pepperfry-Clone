94e06f00b4f3d1e8a24586e0b0b85719799511bf
poojakittu <poojayadav.yadav173@gmail.com>
2023-01-20 19:28:58 +0530

    ade

--- Frontend/src/App.js
+++ Frontend/src/App.js
@@ -1,11 +1,13 @@
 import './App.css';
 import MainRoutes from './AllRoutes/MainRoutes';
 import Navbar from './Components/Navbar';
+import { ProductsPage } from './Components/Product/Productpage';
 
 function App() {
   return (
     <div className="App">
-      <MainRoutes />
+      {/* <MainRoutes /> */}
+      <ProductsPage/>
     </div>
   );
 }
